9d3ad80d0c678d93296a438e63a48b9ef970d761
kamalharsh14 <singhharsh998@gmail.com>
2021-11-01 21:32:01 +0530

    day1

---
+++ kthsmallestelement.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class kthsmallestelement{
+
+    //main funtion
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int n = in.nextInt(); //size of array
+        int arr[] = new int[n];
+
+        //inserting values into the array
+        for(int i = 0 ; i < n ; i ++){
+            arr[i] = in.nextInt();
+        }
+
+        int k = in.nextInt(); //Nth smallest number 
+
+        
+        in.close();
+    }
+}
